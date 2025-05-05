59cac5b331cd3908cdd613d110bc1d64c851d191
novembertwotwo <korea621214@naver.com>
2025-05-05 21:41:14 +0900

    음료수얼려먹기2

---
+++ 음료수얼려먹기2.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+N, M = map(int, input().split(" "))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+graph = list(list(map(int, input())) for _ in range(N))
+
+
+def bfs(x, y):
+    graph[x][y] = 1
+    queue = deque([(x, y)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and not graph[nx][ny]:
+                graph[nx][ny] = 1
+                queue.append((nx, ny))
+
+
+result = 0
+
+for i in range(N):
+    for j in range(M):
+        if not graph[i][j]:
+            bfs(i, j)
+            result += 1
+
+print(result)
